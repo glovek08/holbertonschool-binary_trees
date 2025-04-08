bc3e20730335f12f66f491c50f04172bfcdc380e
854113 <9392@holbertonstudents.com>
2025-04-08 16:39:09 +0000

    task 0 & 1

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
-#ifndef BINARY_TREES.H
-#define BINARY_TREES.H
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 
@@ -12,6 +13,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 struct binary_tree_s
 {
     int n;
@@ -21,11 +23,44 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-/*
+
+/* Binary Tree Print */
+void binary_tree_print(const binary_tree_t *);
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
-*/
 
+/* Binary Tree T */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif /* BINARY_TREES.H */
+/* Voids */
+void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Ints */
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* Sizet's */
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
