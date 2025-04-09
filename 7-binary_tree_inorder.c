f619ad4b67f2e57042be2129d35c646102e800e3
glovek08 <gbrielbbarn@gmail.com>
2025-04-09 20:12:11 -0300

    [feat] 7-binary_tree_inorder.c

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - goes through a binary tree using in-order traversal.
+ * @tree: pointer to the root node of the tree to traverse.
+ * @func: function pointer to call for each node.
+ *
+ * Return: void.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void(*func)(int))
+{
+	const binary_tree_t **stack;
+	const binary_tree_t *current;
+	int top = -1;
+	int max_size = 1000;
+
+	if (!tree || !func)
+		return;
+	stack = malloc(sizeof(binary_tree_t) * max_size);
+	if (!stack)
+		return;
+	current = tree;
+	for (;;)
+	{
+		while (current)
+		{
+			if (top > max_size - 1)
+			{
+				free(stack); /* STACK OVERFLOW!!!! :P */
+				return;
+			}
+			stack[++top] = current;
+			current = current->left;
+		}
+		if (top == -1)
+		{
+			free(stack);
+			return;
+		}
+		current = stack[top--];
+		func(current->n);
+		current = current->right;
+	}
+}
