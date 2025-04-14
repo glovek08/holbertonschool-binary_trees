8c98196b5aed24a95bffb267ab649cf40a0e63cd
glovek08 <gbrielbbarn@gmail.com>
2025-04-13 21:23:16 -0300

    [fix] prevent segmentation fault by first checking if tree is null in 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,15 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	int left_height = 0;
+	int right_height = 0;
 
 	if (!tree)
 		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
 	return (left_height - right_height);
 
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,58 +11,16 @@
  *
  * Return: 1 on success, 0 on queue overflow
  */
-int enqueue_child(const binary_tree_t **queue, size_t *rear,
-		const binary_tree_t *node, size_t queue_size, size_t *next_nodes)
-{
-	if (*rear >= queue_size)
-		return (0);
-	queue[(*rear)++] = node;
-	(*next_nodes)++;
-	return (1);
-}
-
-/**
- * binary_tree_height - measures the height of a binary tree.
- * @tree: pointer to the root node of the b-tree.
- *
- * Return: height of @tree, 0 if @tree is 'NULL'.
- */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0, current_nodes = 0, next_nodes = 0;
-	const binary_tree_t **queue, *current;
-	size_t front = 0, rear = 0, queue_size = 1000;
+    size_t left_height = 0;
+    size_t right_height = 0;
 
-	if (!tree)
-		return (0);
-	queue = malloc(queue_size * sizeof(binary_tree_t *));
-	if (!queue)
-		return (0);
-	queue[rear++] = tree;
-	current_nodes = 1;
-	while (current_nodes > 0)
-	{
-		height++;
-		next_nodes = 0;
-		while (current_nodes > 0)
-		{
-			current = queue[front++];
-			current_nodes--;
-			if (current->left && !enqueue_child(queue, &rear, current->left,
-						queue_size, &next_nodes))
-			{
-				free(queue);
-				return (0);
-			}
-			if (current->right && !enqueue_child(queue, &rear, current->right,
-						queue_size, &next_nodes))
-			{
-				free(queue);
-				return (0);
-			}
-		}
-		current_nodes = next_nodes;
-	}
-	free(queue);
-	return (height - 1);
+    if (!tree)
+	    return(0);
+
+    left_height =  binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + (left_height > right_height ? left_height : right_height));
 }
